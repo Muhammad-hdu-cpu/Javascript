4d03fdba198137ec087e900f8c4c3de42b4d8757
Muhammad Mubarak <taohidsakib9@gmail.com>
2025-02-24 22:16:37 +0600

    done

--- Begginers/01.Basic/console.js
+++ Begginers/01.Basic/console.js
@@ -1,8 +1,10 @@
 //                Console
 console.log("done"); //Out put any valid data in the console.
+console.table({ name: "Mubarak", age: 20 });
+
+
 // alert("hello");
 // //This will only work in web Thats gives a popup.This will give error outside browsers.
-console.table({ name: "Mubarak", age: 20 });
 
 //              Comments
 // comments are those codes ignored by the compiler .
