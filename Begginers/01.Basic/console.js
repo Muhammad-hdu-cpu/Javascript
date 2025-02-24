c800497a9f609bedc6f05524644011cb4869efa9
Muhammad Mubarak <taohidsakib9@gmail.com>
2025-02-24 22:06:24 +0600

    done the setup

--- Begginers/01.Basic/console.js
+++ Begginers/01.Basic/console.js
@@ -1,3 +1,3 @@
 console.log("done"); //Out put any valid data in the console.
-alert("hello"); //This will only work in web Thats gives a popup.This will give error outside browsers.
-
+// alert("hello"); //This will only work in web Thats gives a popup.This will give error outside browsers.
+console.table({name:'Mubarak',age:20})
