02139ea09bb60061c3b95c4a096e1ba4ccdb16a5
Muhammad Mubarak <taohidsakib9@gmail.com>
2025-02-24 22:30:19 +0600

    console done

--- Begginers/01.Basic/console.js
+++ Begginers/01.Basic/console.js
@@ -1,7 +1,7 @@
 //                Console
-console.log("done"); //Out put any valid data in the console.
-console.table({ name: "Mubarak", age: 20 });
-
+console.log("done"); //Output any valid data in the console.
+console.table({ name: "Mubarak", age: 20 }); //Output data as a table
+console.warn('Last warning') 
 
 // alert("hello");
 // //This will only work in web Thats gives a popup.This will give error outside browsers.
--- Begginers/01.Basic/variables.js
+++ Begginers/01.Basic/variables.js
@@ -1,1 +1,19 @@
-               // Variables
+// Variables
+// Variable are like a pot to store data . We can store any kind of datatype in Javascript variables .
+
+// Declare A Variable
+let name;
+console.log(name); //undefined
+// We just declare the variable doesn't store any data thats why this output undefined
+
+// Assign value
+let age = 20;
+console.log(age); //20
+// This time the variable have a value the will log 
+
+
+//Naming a variables 
+// There are some reserve key we can't use them . can't use space,Number as a first character. 
+// We can't use (-) sign instead of use (_).
+
+
