36805b3a7ca4b0e5890e688a403c3b07748cfced
Muhammad Mubarak <taohidsakib9@gmail.com>
2025-02-25 19:00:03 +0600

    comparison operator done

--- Begginers/02.Primitive/Booleans/exercise/app.js
+++ Begginers/02.Primitive/Booleans/exercise/app.js
@@ -1,8 +1,11 @@
+//---------------------------------------Exercise-------------------------------------------------
+
 // 1. Create Variable name (isJsProgrammingLanguage) & store true as a value.
 // 2. Create variable name (isJsHard) & store false as a value.
 // 3. Create variable name (favNumb) & store your favorite number inside.
 // 4. Now Add favNumber with the value of undefined.
 
+//---------------------------------------Answer-------------------------------------------------
 let isJsProgrammingLanguage = true;
 let isJsHard = false;
 let favNumber = 14;
